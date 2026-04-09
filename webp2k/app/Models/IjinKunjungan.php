3c83176587faa19d72486551bd7f36d2e0013899
PutuSugosaAnggaramukti <putu.sugosa@si.ukdw.ac.id>
2026-04-09 12:05:53 +0700

    update oper jadwal 2

--- webp2k/app/Models/IjinKunjungan.php
+++ webp2k/app/Models/IjinKunjungan.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class IjinKunjungan extends Model
 {
     // Tambahkan ini agar data bisa disimpan!
-    protected $fillable = ['karyawan_id','kode_ao', 'tanggal', 'jenis_ijin', 'alasan','status'];
+    protected $fillable = ['karyawan_id','kode_ao', 'tanggal', 'jenis_ijin', 'alasan','status','ao_pengganti'];
 
     public function karyawan()
     {
