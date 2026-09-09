df3f889b645bae4ce8c790fa062e615edb1efa87
PutuSugosaAnggaramukti <putu.sugosa@si.ukdw.ac.id>
2026-09-09 14:45:27 +0700

    fix: gabung Tgl Kunjung ke Catatan Lapangan di Export Nasabah Terkunjungi

--- webp2k/resources/views/admin/exports/nasabah_terkunjungi_excel.blade.php
+++ webp2k/resources/views/admin/exports/nasabah_terkunjungi_excel.blade.php
@@ -1,7 +1,7 @@
 <table>
     <thead>
         <tr>
-            <th colspan="6" style="text-align: center; font-weight: bold; font-size: 14px;">DAFTAR NASABAH SUDAH DIKUNJUNGI</th>
+            <th colspan="5" style="text-align: center; font-weight: bold; font-size: 14px;">DAFTAR NASABAH SUDAH DIKUNJUNGI</th>
         </tr>
         <tr></tr>
         <tr style="background-color: #f5f5f5; text-align: center; font-weight: bold;">
@@ -9,8 +9,7 @@
             <th style="border: 1px solid #000; width: 150px;">No. Angsuran</th>
             <th style="border: 1px solid #000; width: 250px;">Nama Nasabah</th>
             <th style="border: 1px solid #000; width: 200px;">Dikunjungi Oleh (AO)</th>
-            <th style="border: 1px solid #000; width: 150px;">Tgl Kunjung</th>
-            <th style="border: 1px solid #000; width: 300px;">Catatan</th>
+            <th style="border: 1px solid #000; width: 350px;">Catatan Lapangan</th>
         </tr>
     </thead>
     <tbody>
@@ -23,14 +22,19 @@
                         <td style="border: 1px solid #000; text-align: center;">{{ $nasabah->no_angsuran }}</td>
                         <td style="border: 1px solid #000; text-transform: uppercase;">{{ $nasabah->nasabah }}</td>
                         <td style="border: 1px solid #000;">({{ $kunj->kode_ao }}) {{ $kunj->karyawan->nama ?? '-' }}</td>
-                        <td style="border: 1px solid #000; text-align: center;">{{ \Carbon\Carbon::parse($kunj->created_at)->format('d-m-Y') }}</td>
-                        <td style="border: 1px solid #000;">{{ $kunj->catatan ?? '-' }}</td>
+                        <td style="border: 1px solid #000;">
+                            @php
+                                $tanggalKunjungan = \Carbon\Carbon::parse($kunj->created_at)->locale('id')->translatedFormat('d F Y');
+                                $catatan = $kunj->catatan ?? '-';
+                            @endphp
+                            Tanggal {{ $tanggalKunjungan }} {{ $catatan }}
+                        </td>
                     </tr>
                 @endforeach
             @endif
         @empty
             <tr>
-                <td colspan="6" style="border: 1px solid #000; text-align: center; padding: 20px; color: #888;">Belum ada data nasabah yang dikunjungi.</td>
+                <td colspan="5" style="border: 1px solid #000; text-align: center; padding: 20px; color: #888;">Belum ada data nasabah yang dikunjungi.</td>
             </tr>
         @endforelse
     </tbody>
